86b689b5c57ada73f720ac77a2c33b113161576d
Levib52 <59113418+Levib52@users.noreply.github.com>
2019-12-21 03:51:47 -0700

    Update config.php

--- joblister/config/config.php
+++ joblister/config/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define("DB_HOST", "sql204.epizy.com");
-define("DB_USER", "epiz_24873274");
-define("DB_PASS", "z3qnoOtDZV");
-define("DB_NAME", "epiz_24873274_levisjoblister");
+define("DB_HOST", "REDACTED");
+define("DB_USER", "REDACTED");
+define("DB_PASS", "REDACTED");
+define("DB_NAME", "REDACTED");
 
 define("SITE_TITLE", "JobLister");
